a110c81775d0e2761f22b1911bc35a0141f0532d
Alex Wissmann <awiss@wharton.upenn.edu>
2015-09-05 14:03:33 -0400

    S3 uploads and downloads

--- app/route_funcs/datasets.py
+++ app/route_funcs/datasets.py
@@ -1,6 +1,6 @@
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 
-from app.utils.s3 import s3_upload
+from app.utils.s3 import s3_upload, get_s3_url
 
 ALLOWED_EXTENSIONS = ['csv', 'json']
 def allowed_file(filename):
@@ -11,9 +11,12 @@ def new():
     dataset = request.files['dataset']
     if dataset and allowed_file(dataset.filename):
         filename = s3_upload(dataset)
-        return "Success"
-    return "Fail"
+        return redirect(url_for('datasets_view', dataset_name=filename))
+    return redirect(url_for('index'))
 
 def view(dataset_name):
-    return render_template('dataset.html', dataset_name=dataset_name)
+    s3_url = get_s3_url(dataset_name)
+    if not s3_url:
+        return "Dataset does not exist", 404
+    return render_template('dataset.html', download_url=s3_url)
 
