096c726d957088dd93b25bf261eb997c68e517ca
Lauren Reeder <laurenmreeder@gmail.com>
2015-09-05 17:26:17 -0400

    tabs

--- python_testing/mixed_models.py
+++ python_testing/mixed_models.py
@@ -9,50 +9,50 @@ import sys
 
 def naive(filepath):
 
-	def parseLine(line):
-		parts = line.split(',')
-		label = float(parts[0])
-		features = Vectors.dense([float(x) for x in parts[1].split(' ')])
-		return LabeledPoint(label, features)
+    def parseLine(line):
+        parts = line.split(',')
+        label = float(parts[0])
+        features = Vectors.dense([float(x) for x in parts[1].split(' ')])
+        return LabeledPoint(label, features)
 
-	sc = SparkContext("local", "Bayes App")
-	data = sc.textFile(filepath).map(parseLine)
+    sc = SparkContext("local", "Bayes App")
+    data = sc.textFile(filepath).map(parseLine)
 
-	# Split data aproximately into training (60%) and test (40%)
-	training, test = data.randomSplit([0.6, 0.4], seed = 0)
+    # Split data aproximately into training (60%) and test (40%)
+    training, test = data.randomSplit([0.6, 0.4], seed = 0)
 
-	# Train a naive Bayes model.
-	model = NaiveBayes.train(training, 1.0)
+    # Train a naive Bayes model.
+    model = NaiveBayes.train(training, 1.0)
 
-	# Make prediction and test accuracy.
-	predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
-	accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
+    # Make prediction and test accuracy.
+    predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
+    accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
 
-	# print "bayes results... accuracy: {}".format(accuracy)
-	
+    # print "bayes results... accuracy: {}".format(accuracy)
+    
 
 
 # Gaussian mixture - clusters data, but must pass in desired # of clusters
 def gaussian(filepath, n):
-	sc = SparkContext("local", "Gaussian App")
-	data = sc.textFile(filepath)
-	parsedData = data.map(lambda line: array([float(x) for x in line.strip().split(' ')]))
+    sc = SparkContext("local", "Gaussian App")
+    data = sc.textFile(filepath)
+    parsedData = data.map(lambda line: array([float(x) for x in line.strip().split(' ')]))
 
-	# Build the model (cluster the data)
-	gmm = GaussianMixture.train(parsedData, n)
+    # Build the model (cluster the data)
+    gmm = GaussianMixture.train(parsedData, n)
 
-	# output parameters of model
-	for i in range(2):
-		print ("weight = ", gmm.weights[i], "mu = ", gmm.gaussians[i].mu,
-					        "sigma = ", gmm.gaussians[i].sigma.toArray())
+    # output parameters of model
+    for i in range(2):
+        print ("weight = ", gmm.weights[i], "mu = ", gmm.gaussians[i].mu,
+                            "sigma = ", gmm.gaussians[i].sigma.toArray())
   
 
 def main(argv):
   # Check for desired input later... look at length of argv for various models
-  gaussian(argv[1],int(argv[2]))
-#  naive(str(sys.argv[1]))	
+    gaussian(argv[1],int(argv[2]))
+#  naive(str(sys.argv[1]))  
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
 
 
