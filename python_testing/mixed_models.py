298530d72437c171b1321c1d23b9ff4b77639b53
Alex Wissmann <awiss@wharton.upenn.edu>
2015-09-05 22:36:07 -0400

    Merged

--- python_testing/mixed_models.py
+++ python_testing/mixed_models.py
@@ -1,4 +1,8 @@
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS, NaiveBayes, SVMWithSGD, \
+    LogisticRegressionWithSGD
+
 import pyspark.mllib.classification
+from pyspark.mllib.tree import DecisionTree, GradientBoostedTrees
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.context import SparkContext
@@ -10,6 +14,36 @@ from sklearn.metrics import pairwise_distances
 import sys, math, random
 from functools import partial
 
+multinomial_classifiers = {
+    'NaiveBayes': NaiveBayes.train,
+    'LogisticRegressionWithLBFGS': LogisticRegressionWithLBFGS.train,
+    'DecisionTree': DecisionTree.trainClassifier,
+}
+
+binomial_classifiers = {
+    'NaiveBayes': NaiveBayes.train,
+    'SVMWithSGD': SVMWithSGD.train,
+    'LogisticRegressionWithLBFGS': LogisticRegressionWithLBFGS.train,
+    'LogisticRegressionWithSGD': LogisticRegressionWithSGD.train,
+    'DecisionTree': DecisionTree.trainClassifier,
+    'GradientBoostedTrees': GradientBoostedTrees.trainClassifier
+}
+
+with_numClasses = {
+    'LogisticRegressionWithLBFGS',
+    'DecisionTree',
+}
+
+with_categoricalFeatures = {
+    'DecisionTree',
+    'GradientBoostedTrees'
+}
+
+with_regLambda = {
+    'LogisticRegressionWithLBFGS',
+    'LogisticRegressionWithSGD',
+}
+reg_lambda = 0.000001
 
 def train_classifier(data, classifier, train_kwargs={}):
 
@@ -17,36 +51,75 @@ def train_classifier(data, classifier, train_kwargs={}):
     training, test = data.randomSplit([0.6, 0.4], seed = 0)
 
     # Train a classifier
-    model = classifier.train(training, 1.0, **train_kwargs)
+    model = classifier(training, **train_kwargs)
 
     # Make prediction and test accuracy.
-    predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
-    accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
+    predictions = model.predict(test.map(lambda p : p.features))
+    results = predictions.zip(test.map(lambda p : p.label))
+
+    accuracy = 1.0 * results.filter(lambda (x, v): x == v).count() / test.count()
 
     # print "bayes results... accuracy: {}".format(accuracy)
-    print accuracy
-    return model
+    return accuracy
+
+def train_classifier_on_file(file_parser, filepath, classifier_name, num_classes=None):
+
+    sc = SparkContext("local", classifier_name)
+
+    data, num_classes = get_classified_data(sc, file_parser, filepath, num_classes)
+
+    train_classifier_on_data(data, num_classes, classifier_name)
+
+    sc.stop()
+
+def train_classifier_on_data(data, num_classes, classifier_name):
+    train_kwargs = {}
+    if classifier_name in with_numClasses:
+        train_kwargs['numClasses'] = num_classes
+
+    if classifier_name in with_categoricalFeatures:
+        train_kwargs['categoricalFeaturesInfo'] = {}
+
+    if classifier_name in with_regLambda:
+        train_kwargs['regParam'] = reg_lambda
+
+    if num_classes > 2:
+        if classifier_name not in multinomial_classifiers:
+            raise ValueError('classifier_name is invalid. Options are %s'
+                                % multinomial_classifiers.keys())
+
+        return train_classifier(data, multinomial_classifiers[classifier_name],
+                            train_kwargs=train_kwargs)
+
+    elif num_classes == 2:
+        if classifier_name not in binomial_classifiers:
+            raise ValueError('classifier_name is invalid. Options are %s'
+                                % binomial_classifiers.keys())
+
+        return train_classifier(data, binomial_classifiers[classifier_name],
+                            train_kwargs=train_kwargs)
 
 
-def parse_labeled_vectors(sc_file):
-    def parseLine(line):
-        parts = line.split(',')
-        label = float(parts[0])
-        features = Vectors.dense([float(x) for x in parts[1].split(' ')])
-        return LabeledPoint(label, features)
-    return sc_file.map(parseLine)
+def get_classified_data(sc, file_parser, filepath, num_classes=None):
+    data = file_parser(sc.textFile(filepath))
+    if num_classes is None:
+        labels = data.groupBy(lambda x: x.label).collect()
+        num_classes = len(labels)
+    if num_classes < 2:
+        raise ValueError('Number of classes in dataset must be greater than 1')
+    return data, num_classes
 
 
-def train_on_file(file_parser, filepath, classifier_name, num_classes=2):
-    print "NAME:", classifier_name
-    if hasattr(pyspark.mllib.classification, classifier_name):
-        sc = SparkContext("local", "Bayes App")
-        data = file_parser(sc.textFile(filepath))
-        train_classifier(data, getattr(pyspark.mllib.classification, classifier_name),
-                         train_kwargs={'numClasses':num_classes})
-        sc.stop()
+def get_classifier_results(file_parser, filepath, num_classes=None):
+    sc = SparkContext("local", "Classifier Performance")
+    data, num_classes = get_classified_data(sc, file_parser, filepath, num_classes)
+    classifiers = binomial_classifiers if num_classes == 2 else multinomial_classifiers
+    accuracies = map(lambda class_name: (class_name, train_classifier_on_data(data, num_classes,
+                                                                              class_name)),
+                     classifiers.keys())
+    sc.stop()
+    print accuracies
 
-train_classifier_on_file = partial(train_on_file, parse_labeled_vectors)
 
 # Gaussian mixture - clusters data, but must pass in desired # of clusters
 def gaussian(filepath, n):
@@ -61,7 +134,7 @@ def gaussian(filepath, n):
     for i in range(2):
         print ("weight = ", gmm.weights[i], "mu = ", gmm.gaussians[i].mu,
                             "sigma = ", gmm.gaussians[i].sigma.toArray())
-   
+
     # Metric for unsupervised - silhouette coefficient, higher numbers ~ better fit
     labels = gmm.predict(parsedData).collect()
     features = np.array(map(lambda x : x.strip().split(" "), data.collect()))
@@ -69,14 +142,39 @@ def gaussian(filepath, n):
     score = metrics.silhouette_score(features, np.array(labels), metric='euclidean')
     print "score: {}".format(score)
 
+def parse_labeled_vectors(sc_file):
+    def parseLine(line):
+        parts = line.split(',')
+        label = float(parts[0])
+        features = Vectors.dense([float(x) for x in parts[1].split(' ')])
+        return LabeledPoint(label, features)
+    return sc_file.map(parseLine)
+
+def parse_csv(sc_file):
+    def parseLine(line):
+        parts = line.split(',')
+        label = float(parts[-1])
+        features = Vectors.dense([float(x) for x in parts[:-1]])
+        return LabeledPoint(label, features)
+    return sc_file.map(parseLine)
+
+train_classifier_on_labeled_vectors = partial(train_classifier_on_file, parse_labeled_vectors)
+results_on_labeled_vectors = partial(get_classifier_results, parse_labeled_vectors)
 
 def main(argv):
   # Check for desired input later... look at length of argv for various models
 #    gaussian(argv[1],int(argv[2]))
+    print sys.argv
     if sys.argv[1] == 'classifier':
-        train_classifier_on_file(str(sys.argv[3]), str(sys.argv[2]), int(sys.argv[4]))
-
-
+        if len(sys.argv) == 5:
+            train_classifier_on_labeled_vectors(str(sys.argv[3]),
+                                                str(sys.argv[2]),
+                                                int(sys.argv[4]))
+        elif len(sys.argv) == 4:
+            train_classifier_on_labeled_vectors(str(sys.argv[3]),
+                                                str(sys.argv[2]))
+        elif len(sys.argv) == 3:
+            get_classifier_results(parse_csv, str(sys.argv[2]))
 if __name__ == '__main__':
     main(sys.argv)
 
